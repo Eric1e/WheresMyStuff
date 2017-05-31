a68881051ed1bb12eb41c34ecff816f7f114b954
Eric1e <esheen11@gmail.com>
2017-05-31 16:27:58 -0400

    Update Person1.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Eric Sheen
  *  @version 1.1
  */
 public class Person1 {
@@ -30,8 +30,9 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+		String first = input.substring(0, 2);
+		String last = input.substring(2, input.length());
+		return last + first;
 	}
 	
 	/**
